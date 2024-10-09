c415d6eaecd47f9b672de6ca01b8e7b5819f3b28
KZLenin <131980094+kzlenin@users.noreply.github.com>
2024-10-09 04:44:13 -0500

    Agreado un while para el retorno al menu ademas el area del circulo y cuadrado uso de funcion Math para el número pi y las potencias

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("-------Calculadora de Areas-------");
+            System.out.println("1) Area del triangulo: ");
+            System.out.println("2) Area del cuadrado: ");
+            System.out.println("3) Area del circulo: ");
+            System.out.println("4) Salir del sistema");
+            System.out.println("Ingrese la figura que desea calcular: ");
+            int figura = sc.nextInt();
+            switch (figura) {
+                case 1:
+                    System.out.println("---Area del triangulo---");
+                    System.out.println("Ingrese la base del triangulo: ");
+                    double base = sc.nextDouble();
+                    System.out.println("Ingrese la altura del triangulo: ");
+                    double altura = sc.nextDouble();
+                    double area = base * altura / 2.0;
+                    System.out.println("El area del triangulo es: " + area);
+                    break;
+                case 2:
+                    System.out.println("---Area del cuadrado---");
+                    System.out.println("Ingrese un lado del cuadrado: ");
+                    double ladoC = sc.nextDouble();
+                    double areaC = Math.pow(ladoC, 2.0);
+                    System.out.println("El area del cuadrado es: " + areaC);
+                    break;
+                case 3:
+                    System.out.println("---Area del circulo---");
+                    System.out.println("Ingrese el radio del circulo: ");
+                    double radioCr = sc.nextDouble();
+                    double areaCr = Math.PI * Math.pow(radioCr, 2.0);
+                    System.out.println("El area del circulo es: " + areaCr);
+                    break;
+                case 4:
+                    System.out.println("---Saliendo del programa---");
+                    return;
+                default:
+                    System.out.println("---Ingrese un número que se encuentre en el menu---");
+            }
+
+            System.out.println("Presione enter para volver al menu");
+            sc.nextLine();
+            sc.nextLine();
         }
     }
 }
