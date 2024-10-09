54ba9bf898974152084dfc52a4c96f3627729abe
KZLenin <131980094+KZLenin@users.noreply.github.com>
2024-10-09 04:44:13 -0500

    Agregados Menú y area del triangulo

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-------Calculadora de Areas-------");
+        System.out.println("1) Area del triangulo: ");
+        System.out.println("2) Area del cuadrado: ");
+        System.out.println("3) Area del circulo: ");
+        System.out.println("Ingrese la figura que desea calcular: ");
+        int figura = sc.nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        switch (figura) {
+            case 1:
+                System.out.println("---Area del triangulo---");
+                System.out.println("Ingrese la base del triangulo: ");
+                double base = sc.nextDouble();
+                System.out.println("Ingrese la altura del triangulo: ");
+                double altura = sc.nextDouble();
+                double area = (base*altura)/2;
+                System.out.println("El area del triangulo es: " + area);
+                break;
+            case 2:
+                System.out.println("---Area del cuadrado---");
+                System.out.println("Ingrese un lado del cuadrado: ");
         }
     }
 }
